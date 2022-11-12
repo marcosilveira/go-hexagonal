a4a66a50d3980c413ffd4d7658f7ac8a0b9cccf5
Marco Silveira <marcosilveira39@gmail.com>
2022-11-12 19:42:18 -0300

    second commit

---
+++ application/adapters/web/server/server.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/marcosilveira/go-hexagonal/application"
+	"github.com/marcosilveira/go-hexagonal/application/adapters/web/handler"
+)
+
+type Webserver struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebserver() *Webserver {
+	return &Webserver{}
+}
+
+func (w Webserver) Serve() {
+	r := mux.NewRouter()
+	n := negroni.New(negroni.NewLogger())
+
+	handler.MakeProductHandlers(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":8080",
+		Handler:      http.DefaultServeMux,
+		ErrorLog:     log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
